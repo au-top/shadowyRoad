abc9bcfb6cf6e3394edd2366bb0383a30c84744b
autop <44134842+auto-p@users.noreply.github.com>
2020-07-19 17:58:57 +0800

    Add files via upload

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ mkdir ./build/dist/
 cp ./src/config.json ./build/config.json
 cp ./src/config.json ./build/dist/config.json
 (
-    npx webpack ./build/O.js --target=node --output-path=./build/dist/ --output-filename=O.js --mode=production 
-    npx webpack ./build/Ol.js --target=node --output-path=./build/dist/ --output-filename=Ol.js --mode=production 
+    npx webpack ./build/O.js --target=node --output-path=./build/dist/ --output-filename=O.js --mode=production  --config=./webpack.config.js
+    npx webpack ./build/Ol.js --target=node --output-path=./build/dist/ --output-filename=Ol.js --mode=production --config=./webpack.config.js
 )
